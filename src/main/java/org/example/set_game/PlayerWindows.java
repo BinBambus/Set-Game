0a919a2daba77957e9be52bcf7939b42f0860e34
Maq911 <maq911@hs-mainz.de>
2024-05-24 19:19:50 +0200

    Funktionierender Timer beim Spieler

--- src/main/java/org/example/set_game/PlayerWindows.java
+++ src/main/java/org/example/set_game/PlayerWindows.java
@@ -1,5 +1,7 @@
 package org.example.set_game;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -8,18 +10,23 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.util.Timer;
 
 public class PlayerWindows extends Application {
 
     private int player = 1;
-    private int malte = 1;
+    private int playerTimer = 15;
+    private int playerScore = 0;
+    Timeline timeline;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage playerStage) {
-        malte = malte+1;
         //Setting Root
         VBox vbox = new VBox();
         vbox.setAlignment(Pos.CENTER);
@@ -31,16 +38,19 @@ public class PlayerWindows extends Application {
         //Buttons and Labels
 
         // Nur fürs Beispiel
-        int playerScore = 0;
         Label scoreLabel = new Label("Score: " +playerScore);
 
 
         // Nur fürs Beispiel
-        int playerTimer = 15;
         Label timeLeftLabel = new Label("Time Left: "+playerTimer);
 
 
+
+
         Button playerSetButton = new Button("SET");
+        playerSetButton.setOnAction(e -> {
+            start1orEnd0_PlayerTimer(true, scoreLabel, timeLeftLabel);
+        });
 
 
         //SetStage
@@ -54,4 +64,30 @@ public class PlayerWindows extends Application {
     public void setPlayer(int player){
         this.player = player;
     }
+
+    //Starting the Timer after SET-Button is pressed
+    public void start1orEnd0_PlayerTimer(boolean endOrStart, Label scoreLabel, Label timeLeftLabel){
+        timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+            if(playerTimer > 0){
+                playerTimer--;
+                timeLeftLabel.setText("Time Left: " + playerTimer);//Update Timer
+            } else{
+                playerTimer = 15;
+                playerScore--;
+                scoreLabel.setText("Score: " + playerScore);//Update Score if time runs out (Score -1)
+                timeLeftLabel.setText("Time Left: " + playerTimer);//Update Timer
+                timeline.stop();
+            }
+
+        }));
+        if(endOrStart){
+            timeline.setCycleCount(Timeline.INDEFINITE); // Run indefinitely
+            timeline.play(); // Start the timeline
+        }else{
+            timeline.stop();
+            playerTimer = 15;
+        }
+
+    }
+
 }
